50355eaab2ae42e327a06380d975fae9ab33e19b
Eric Holscher <eric@ericholscher.com>
2010-02-21 22:28:03 -0600

    Make the pony_utils test runner 1.2 compatible.

--- pony_utils/django/test_runner.py
+++ pony_utils/django/test_runner.py
@@ -1,62 +1,17 @@
-import unittest
-from django.conf import settings
-from django.db.models import get_app, get_apps
-from django.test import _doctest as doctest
-from django.test.utils import setup_test_environment, teardown_test_environment
-from django.test.testcases import OutputChecker, DocTestRunner, TestCase
-from django.test.simple import *
-
+from django.test.simple import DjangoTestSuiteRunner
 from pony_utils.django.utils import report_results_for_suite
 
-def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
-    """
-    Run the unit tests for all the test labels in the provided list.
-    Labels must be of the form:
-     - app.TestClass.test_method
-        Run a single specific test method
-     - app.TestClass
-        Run all the test methods in a given class
-     - app
-        Search for doctests and unittests in the named application.
-
-    When looking for tests, the test runner will look in the models and
-    tests modules for the application.
-
-    A list of 'extra' tests may also be provided; these tests
-    will be added to the test suite.
-
-    Returns the number of tests that failed.
-    """
-    setup_test_environment()
-
-    settings.DEBUG = False
-    suite = unittest.TestSuite()
-
-    if test_labels:
-        for label in test_labels:
-            if '.' in label:
-                suite.addTest(build_test(label))
-            else:
-                app = get_app(label)
-                suite.addTest(build_suite(app))
-    else:
-        for app in get_apps():
-            #run_tests(build_suite(app))
-            suite.addTest(build_suite(app))
-
-    for test in extra_tests:
-        suite.addTest(test)
-
-    suite = reorder_suite(suite, (TestCase,))
-
-    old_name = settings.DATABASE_NAME
-    from django.db import connection
-    connection.creation.create_test_db(verbosity, autoclobber=not interactive)
-    result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
-    connection.creation.destroy_test_db(old_name, verbosity)
-
-    teardown_test_environment()
-
-    report_results_for_suite(suite, result)
-
-    return len(result.failures) + len(result.errors)
+class PonyReportRunner(DjangoTestSuiteRunner):
+    def run_tests(self, test_labels, extra_tests=None):
+        """
+        Run the Django test suite and report results to pony_server.
+        """
+        self.setup_test_environment()
+        suite = self.build_suite(test_labels, extra_tests)
+        old_config = self.setup_databases()
+        result = self.run_suite(suite)
+        self.teardown_databases(old_config)
+        self.teardown_test_environment()
+        #This is the only line changed.
+        report_results_for_suite(suite, result)
+        return self.suite_result(result)
