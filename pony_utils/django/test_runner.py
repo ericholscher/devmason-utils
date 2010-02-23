abc87dae6c087e629c1fdd34ac31cef1fe1c48ca
Default Virtual Server <jill@ff9z7saa.joyent.us>
2010-02-23 03:41:59 +0000

    Make the 1.2 version of the code even awesomer.

--- pony_utils/django/test_runner.py
+++ pony_utils/django/test_runner.py
@@ -2,16 +2,6 @@ from django.test.simple import DjangoTestSuiteRunner
 from pony_utils.django.utils import report_results_for_suite
 
 class PonyReportRunner(DjangoTestSuiteRunner):
-    def run_tests(self, test_labels, extra_tests=None):
-        """
-        Run the Django test suite and report results to pony_server.
-        """
-        self.setup_test_environment()
-        suite = self.build_suite(test_labels, extra_tests)
-        old_config = self.setup_databases()
-        result = self.run_suite(suite)
-        self.teardown_databases(old_config)
-        self.teardown_test_environment()
-        #This is the only line changed.
+    def suite_result(self, suite, result, **kwargs): 
         report_results_for_suite(suite, result)
         return self.suite_result(result)
