862366a27b818ce7452030ebd49d822bc75185c1
Eric Holscher <eric@ericholscher.com>
2010-03-10 21:11:51 -0600

    Added super basic tests, requires a local server to be running.

---
+++ devmason_utils/tests.py
@@ -0,0 +1,11 @@
+import pprint
+import difflib
+from django.test import TestCase, Client
+
+from devmason_utils.utils import create_package, get_auth_string
+
+class CreatePackageTests(TestCase):
+
+    def test_create_package(self):
+        auth = get_auth_string('test', 'test')
+        resp = create_package('My Project2', server='http://localhost:8000/devmason', auth=auth)
